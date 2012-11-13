172b5c23a232bfc877be89a1ada67e9fa9501eb0
anrugas <anrugas@localhost>
2012-11-13 14:20:54 -0600

    Add GraphicsMagick

---
+++ data/graphicsmagick-atv/make.sh
@@ -0,0 +1,17 @@
+echo PKG_ROOT = "${PKG_ROOT}"
+echo PKG_WORK = "${PKG_WORK}"
+echo PKG_DATA = "${PKG_DATA}"
+echo PKG_DEST = "${PKG_DEST}"
+echo PKG_TARG = "${PKG_TARG}"
+
+pkg:setup
+
+export CPPFLAGS="-I$(PKG_DEST_ freetype2-atv)/usr/include/freetype2 -I$(PKG_DEST_ libxml2)/usr/include/libxml2"
+
+export PATH=$PATH:"$(PKG_DEST_ libxml2)"/usr/bin
+
+pkg:configure ac_cv_path_xml2_config="$(PKG_DEST_ libxml2)"/usr/bin/xml2-config --disable-largefile
+
+pkg:make
+
+pkg:install
