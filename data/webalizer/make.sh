6b2a23f04aa60f9a131f66e2f3a3ad3be2872456
anrugas <anrugas@localhost>
2013-03-12 18:58:34 -0500

    webalizer: moved to Webalizer Xtended RB29

--- data/webalizer/make.sh
+++ data/webalizer/make.sh
@@ -5,9 +5,13 @@ echo PKG_DEST = "${PKG_DEST}"
 echo PKG_TARG = "${PKG_TARG}"
 
 pkg:setup
-export LDFLAGS="-L"$(PKG_DEST_ _libgd)"/usr/lib -L"$(PKG_DEST_ _libpng)"/usr/lib -L"$(PKG_DEST_ _libjpeg)"/usr/lib -L"$(PKG_DEST_ berkeleydb)"/usr/lib -L"$(PKG_DEST_ _libgeoip)"/usr/lib"
-pkg:configure --prefix=/usr/local --enable-bz2 ac_cv_lib_bz2_main --sysconfdir=/etc --enable-dns ac_cv_lib_z_main=yes ac_cv_lib_png_main=yes ac_cv_lib_z_gzrewind=yes --with-png="$(PKG_DEST_ _libpng)"/usr/include --enable-geoip --with-geoip="$(PKG_DEST_ _libgeoip)"/usr/include --with-gd="$(PKG_DEST_ _libgd)"/usr/include ac_cv_lib_gd_main=yes ac_cv_lib_gd_gdImagePng=yes --with-db="$(PKG_DEST_ berkeleydb)"/usr/include
+
+export LDFLAGS="-L"$(PKG_DEST_ _libgd)"/usr/local/lib -L"$(PKG_DEST_ _libpng)"/usr/lib -L"$(PKG_DEST_ _libjpeg)"/usr/lib -L"$(PKG_DEST_ berkeleydb)"/usr/lib -L"$(PKG_DEST_ _libgeoip)"/usr/local/lib"
+pkg:configure --prefix=/usr/local --with-geodb=/usr/local/share/GeoDB/ --enable-bz2 ac_cv_lib_bz2_main --sysconfdir=/etc --enable-dns ac_cv_lib_z_main=yes ac_cv_lib_png_main=yes ac_cv_lib_z_gzrewind=yes --with-png="$(PKG_DEST_ _libpng)"/usr/include --enable-geoip --with-geoip="$(PKG_DEST_ _libgeoip)"/usr/local/include --with-gd="$(PKG_DEST_ _libgd)"/usr/local/include ac_cv_lib_gd_main=yes ac_cv_lib_gd_gdImagePng=yes --with-db="$(PKG_DEST_ berkeleydb)"/usr/include
 make  
 pkg:install
 rm "${PKG_DEST}"/etc/webalizer.conf.sample 
 cp "${PKG_DATA}"/webalizer.conf "${PKG_DEST}"/etc/webalizer.conf
+
+mkdir -p "${PKG_DEST}"/var/www/webalizer
+cp "${PKG_DATA}"/tk.webstack.ios.index.html "${PKG_DEST}"/var/www/webalizer
