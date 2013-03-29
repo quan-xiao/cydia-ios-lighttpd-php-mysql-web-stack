568bcc0729602ef51b0cc2bbcea63a4887d0258e
anrugas <anrugas@localhost>
2013-03-29 18:24:55 -0500

    Add new package logrotate (v3.8.3)

---
+++ data/logrotate/make.sh
@@ -0,0 +1,17 @@
+echo PKG_ROOT = "${PKG_ROOT}"
+echo PKG_WORK = "${PKG_WORK}"
+echo PKG_DATA = "${PKG_DATA}"
+echo PKG_DEST = "${PKG_DEST}"
+echo PKG_TARG = "${PKG_TARG}"
+
+pkg:setup
+export CC=arm-apple-darwin9-gcc
+export POPT_DIR="$(PKG_DEST_ _libpopt)"/usr/local
+
+make  
+
+mkdir -p "${PKG_DEST}"/usr/local/sbin
+mkdir -p "${PKG_DEST}"/etc
+chmod 755 logrotate
+cp logrotate "${PKG_DEST}"/usr/local/sbin 
+cp "${PKG_DATA}"/logrotate.conf "${PKG_DEST}"/etc
