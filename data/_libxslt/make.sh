3fe08b0561fd9c9d32a4c4a5ed1e27ff10328827
anrugas <anrugas@localhost>
2013-04-16 16:07:14 -0500

    _libxslt: adjust make.sh.  ./configure have to use the telesphoreo libxml2 headers instead of the system libxml2 headers

--- data/_libxslt/make.sh
+++ data/_libxslt/make.sh
@@ -1,7 +1,12 @@
+echo PKG_ROOT = "${PKG_ROOT}"
+echo PKG_WORK = "${PKG_WORK}"
+echo PKG_DATA = "${PKG_DATA}"
+echo PKG_DEST = "${PKG_DEST}"
+echo PKG_TARG = "${PKG_TARG}"
+
+
 pkg:setup
 
-pkg:configure --with-xml --without-python --host=arm-apple-darwin9
-
+pkg:configure --with-xml --without-python --host=arm-apple-darwin9 --with-libxml-prefix="$(PKG_DEST_ _libxml2)"/usr --with-libxml-include-prefix="$(PKG_DEST_ _libxml2)"/usr/include/libxml2 --with-libxml-libs-prefix="$(PKG_DEST_ _libxml2)"/usr/lib
 pkg:make
 make install prefix="${PKG_DEST}"/usr datarootdir="${PKG_DEST}"/usr
-
