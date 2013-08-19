800830a44ecc7606702b6a2bb67e182dcd6d7317
anrugas <anrugas@localhost>
2013-08-19 15:56:47 -0500

    Add new packages gnupg. It updates Sauriks older and unsafe version.

---
+++ data/gnupg/make.sh
@@ -0,0 +1,8 @@
+pkg:setup
+#pkg:configure --disable-nls 
+pkg:configure --disable-endian-check --with-libcurl="$(PKG_DEST_ curl)"/usr/lib --disable-nls --disable-card-support --with-included-regex
+
+mkdir -p "${PKG_DEST}"/usr/local/share/info/dir
+make
+pkg:install
+pkg: rm -f /usr/share/gnupg/{FAQ,faq.html}
