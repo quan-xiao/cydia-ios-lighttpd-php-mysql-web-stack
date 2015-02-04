49fdbafc005cb2a1f96f2767ba2b57f58c97f6fe
anrugas <anrugas@localhost>
2015-02-04 07:18:47 -0600

    Add new package ZNC

---
+++ data/tk.ios-webstack.znc/make.sh
@@ -0,0 +1,12 @@
+echo PKG_ROOT = "${PKG_ROOT}"
+echo PKG_WORK = "${PKG_WORK}"
+echo PKG_DATA = "${PKG_DATA}"
+echo PKG_DEST = "${PKG_DEST}"
+echo PKG_TARG = "${PKG_TARG}"
+
+pkg:setup
+
+pkg:configure --prefix=/usr/local
+pkg:make  
+pkg:install
+
