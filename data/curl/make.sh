006a6aa105ef6ae7c24153d3f97cee9bce0f190b
anrugas <anrugas@localhost>
2015-03-04 18:17:24 -0600

    curl: update to version 7.41.0

--- data/curl/make.sh
+++ data/curl/make.sh
@@ -1,7 +1,11 @@
 pkg:setup
 autoconf-2.69
 
-pkg:configure --with-random=/dev/urandom --enable-ipv6 --with-openssl --with-ca-path=/etc/ssl/certs
+pkg:configure --with-random=/dev/random --enable-ipv6 --with-openssl --with-ca-path=/etc/ssl/certs
 make test
 
 pkg:install
+
+#logrotate config files
+mkdir -p ${PKG_DEST}/etc/logrotate.d
+cp ${PKG_DATA}/logrotate_curl ${PKG_DEST}/etc/logrotate.d/curl
